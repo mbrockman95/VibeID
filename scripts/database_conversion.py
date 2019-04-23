d0d2f89a7b74fa6f93b59833e0c9c7e42d98243d
Seesawkayak <38901738+Seesawkayak@users.noreply.github.com>
2019-04-23 16:12:29 -0700

    the VibeID file can download mp3 convert to wav and plot as spectrogram for batches of files based on the download convert query fixed issue with database conversion issue

--- scripts/database_conversion.py
+++ scripts/database_conversion.py
@@ -5,10 +5,8 @@ def download():
     client.set_token("8B01Qr77J2Z911rWyOz852g5mZOo4fJxBiw23LMC","token")
 
     results = client.text_search(query="dubstep",fields="id,name,previews")
-return results
 
-print(results)
-#for sound in results:
- #       sound.retrieve_preview(".",sound.name+".mp3")
-  #      print(sound.name+"-downloaded")
+    for sound in results:
+        sound.retrieve_preview(".",sound.name+".mp3")
+        print(sound.name+"-downloaded")
     
